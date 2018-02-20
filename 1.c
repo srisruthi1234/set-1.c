bf14b30e65beed56638697475e623e5b4f528acc
srisruthi1234 <srisruthi147@gmail.com>
2018-02-20 10:14:20 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>0 && a<=100000)
+{
+printf("postive");
+}
+else if(a==0)
+{
+printf("\nZero");
+}
+else
+{
+printf("Negative\n");
+}
+return o;
+}
